6c2c0ecaa712ac94831e631b340867eea1bdc59e
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-01-11 17:29:54 +0900

    파일 변경으로 인한 재커밋

---
+++ src/Class/ClassStart1.java
@@ -0,0 +1,18 @@
+package Class;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+
+        // 학생 한 명을 더 추가하기 위해서는 하나하나씩 입력해야 하는 번거로움이 있음.
+
+        System.out.println("이름 : " + student1Name + " 나이 : " + student1Age + " 성적 : " + student1Grade);
+        System.out.println("이름 : " + student2Name + " 나이 : " + student2Age + " 성적 : " + student2Grade);
+    }
+}
