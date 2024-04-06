106f90c71e41a4cfa49c78436755ba0dacac0a70
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-04-07 01:17:24 +0900

    2024.04.07 Section7. Static edit

--- src/static2/ex/MathArrayUtils.java
+++ src/static2/ex/MathArrayUtils.java
@@ -3,20 +3,53 @@ package static2.ex;
 import java.util.Arrays;
 
 public class MathArrayUtils {
+    
+    private MathArrayUtils() {
+        //private 인스턴스 생성을 막는다.
+    }
+    
 
     public static int sum(int[] valeus) {
         return Arrays.stream(valeus).sum();
+        
+        // 정답 코드
+//        int total = 0;
+//        for (int value : valeus) {
+//            total += value;
+//        }
+//        return total;
     }
 
     public static double average(int[] values) {
+        
         return (double) sum(values) / values.length;
     }
 
     public static int min(int[] values) {
         return Arrays.stream(values).min().getAsInt();
+
+        // 정답 코드
+//        int minValue = values[0];
+//        for (int value : values
+//        ) {
+//            if (value < minValue) {
+//                minValue = value;
+//            }
+//        }
+//        return minValue;
     }
 
     public static int max(int[] values) {
         return Arrays.stream(values).max().getAsInt();
+
+        // 정답 코드
+//        int maxValue = values[0];
+//        for (int value : values
+//        ) {
+//            if (value > maxValue) {
+//                maxValue = value;
+//            }
+//        }
+//        return maxValue;
     }
 }
