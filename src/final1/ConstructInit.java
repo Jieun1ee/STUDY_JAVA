75064abcc701fdb55ca3dd836c384b31a1c66249
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-04-09 01:38:05 +0900

    2024.04.09 Section8. Final

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value; //초기값 없는 경우 -> 생성자로 초기값 주기
+
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,7 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10; //초기값 있는 경우
+}
