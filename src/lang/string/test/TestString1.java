0b6751f203bce60e573684f256aa22c57cd30ca6
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-05-15 15:34:31 +0900

    2024.05.15 중급1편 Section3.String 클래스

---
+++ src/lang/string/test/TestString1.java
@@ -0,0 +1,10 @@
+package lang.string.test;
+
+public class TestString1 {
+
+    public static void main(String[] args) {
+        String url1 = "https://www.example.com";
+        boolean bool = url1.startsWith("https://");
+        System.out.println(bool);
+    }
+}
